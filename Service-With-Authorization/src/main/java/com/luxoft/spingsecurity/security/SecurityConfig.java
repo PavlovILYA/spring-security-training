3d4d310100a8d2420fb2ec048e1bcc336ea489a6
Илюша <79216023478@yandex.ru>
2024-03-26 13:42:24 +0300

    add UserDetailsService

--- Service-With-Authorization/src/main/java/com/luxoft/spingsecurity/security/SecurityConfig.java
+++ Service-With-Authorization/src/main/java/com/luxoft/spingsecurity/security/SecurityConfig.java
@@ -1,13 +1,19 @@
 package com.luxoft.spingsecurity.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private UserDetailsService userDetailsService;
 
     @Override
     public void configure(WebSecurity web) {
@@ -28,5 +34,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .httpBasic();
         // @formatter:on
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService)
+                .passwordEncoder(NoOpPasswordEncoder.getInstance());
     }
 }
---
+++ Service-With-Authorization/src/main/java/com/luxoft/spingsecurity/security/UserDetailsServiceImpl.java
@@ -0,0 +1,20 @@
+package com.luxoft.spingsecurity.security;
+
+import com.luxoft.spingsecurity.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+        return new UserDetailsAdapter(userRepository.findUserByLogin(login)
+                .orElseThrow(() -> new UsernameNotFoundException(login)));
+    }
+}
